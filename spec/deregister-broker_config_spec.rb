905b9a9e51294bd44e34714c35ce80f090ea40b0
Winna Bridgewater <wbridgewater@pivotal.io>
2017-09-20 14:06:03 +0100

    Updated deregister-broker errand specs
    [#146152105]
    
    Signed-off-by: Jonathan Tsang <jtsang@pivotal.io>

---
+++ spec/deregister-broker_config_spec.rb
@@ -0,0 +1,86 @@
+# Copyright (C) 2016-Present Pivotal Software, Inc. All rights reserved.
+# This program and the accompanying materials are made available under the terms of the under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+require 'spec_helper'
+require 'tempfile'
+
+RSpec.describe 'deregister-broker config' do
+  let(:renderer) do
+    merged_context = BoshEmulator.director_merge(
+      YAML.load_file(manifest_file.path),
+      'deregister-broker'
+    )
+    merged_context['links'] = {
+      'broker' => broker_link
+    }
+
+    Bosh::Template::Renderer.new(context: merged_context.to_json)
+  end
+
+  let(:rendered_template) do
+    renderer.render('jobs/deregister-broker/templates/config.yml.erb')
+  end
+
+  context'with the broker link' do
+    let(:manifest_file) { File.open 'spec/fixtures/deregister_broker.yml' }
+
+    let(:broker_link) do
+      {
+        'instances' => [{}],
+        'properties' => {
+          'disable_ssl_cert_verification' => true,
+          'cf' => {
+            'url' => "https://api.cf-app.com",
+            'root_ca_cert' => 'cert',
+            'authentication' => {
+              'url' => 'https://uaa.cf-app.com',
+              'client_credentials' => {
+                'client_id' => 'some_client_id',
+                'secret' => 'some_secret'
+              },
+              'user_credentials' => {
+                'username' => 'some-username',
+                'password' => 'some-password'
+              }
+            }
+          }
+        }
+      }
+    end
+
+    let(:config) { YAML.load(rendered_template) }
+
+    it 'is not empty' do
+      expect(config).not_to be_empty
+    end
+
+    it 'sets disable_ssl_cert_verification' do
+      expect(config.fetch('disable_ssl_cert_verification')).to eq(true)
+    end
+
+    it 'sets the cf url' do
+      expect(config.fetch('cf').fetch('url')).to eq('https://api.cf-app.com')
+    end
+
+    it 'sets the cf root_ca_cert' do
+      expect(config.fetch('cf').fetch('root_ca_cert')).to eq('cert')
+    end
+
+    it 'sets the cf authentication url' do
+      expect(config.fetch('cf').fetch('authentication').fetch('url')).to eq('https://uaa.cf-app.com')
+    end
+
+    it 'sets the cf authentication client_credentials' do
+      expect(config.fetch('cf').fetch('authentication').fetch('client_credentials').fetch('client_id')).to eq('some_client_id')
+      expect(config.fetch('cf').fetch('authentication').fetch('client_credentials').fetch('secret')).to eq('some_secret')
+    end
+
+    it 'sets the cf authentication user_credentials' do
+      expect(config.fetch('cf').fetch('authentication').fetch('user_credentials').fetch('username')).to eq('some-username')
+      expect(config.fetch('cf').fetch('authentication').fetch('user_credentials').fetch('password')).to eq('some-password')
+    end
+  end
+end
---
+++ spec/deregister-broker_errand_spec.rb
@@ -0,0 +1,25 @@
+# Copyright (C) 2016-Present Pivotal Software, Inc. All rights reserved.
+# This program and the accompanying materials are made available under the terms of the under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+require 'spec_helper'
+
+RSpec.describe 'deregister-broker errand' do
+  let(:renderer) { Bosh::Template::Renderer.new(context: BoshEmulator.director_merge(YAML.load_file(manifest_file), 'deregister-broker').to_json) }
+
+  let(:rendered_template) { renderer.render('jobs/deregister-broker/templates/errand.sh.erb') }
+
+  context 'broker name is set' do
+    let(:manifest_file) { 'spec/fixtures/delete_all_and_deregister_broker_name_is_set.yml' }
+
+    it 'errand calls deregister-broker binary with config and broker name' do
+      broker_name = 'test-broker'
+      binary_call = "/var/vcap/packages/deregister-broker/bin/deregister-broker \\\n" +
+      "  -configFilePath /var/vcap/jobs/deregister-broker/config/config.yml \\\n  -brokerName #{broker_name}"
+
+      expect(rendered_template).to include binary_call
+    end
+  end
+end
