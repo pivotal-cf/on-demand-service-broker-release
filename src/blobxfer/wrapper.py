66f906688ab16a8c0ab4c6d60fc0426c707c0baa
Andrea Nodari <anodari@pivotal.io>
2017-12-21 12:26:04 +0000

    Update blobxfer to version 1.0.0
    [#153317656]
    
    Signed-off-by: Kieron Browne <kbrowne@pivotal.io>

--- src/blobxfer/wrapper.py
+++ src/blobxfer/wrapper.py
@@ -1,18 +1,13 @@
-#!/var/vcap/packages/service-backup_blobxfer/bin/python
-
-# Copyright (C) 2016-Present Pivotal Software, Inc. All rights reserved.
-# This program and the accompanying materials are made available under the terms of the under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-
+#!/var/vcap/packages/service-backup_blobxfer/bin/python3
 
 # -*- coding: utf-8 -*-
 import re
 import sys
 
-from blobxfer import main
+sys.path.append('/var/vcap/packages/service-backup_blobxfer/lib/python3.6/site-packages')
+
+from blobxfer_cli.cli import cli
 
 if __name__ == '__main__':
-    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    sys.exit(main())
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(cli())
