aa7f0ed25c9fa90084e1452c556ed032b26204a2
Josh Hill <jhill@pivotal.io>
2018-02-28 09:58:46 +0000

    Use official bosh template test helpers in control script tests

--- spec/ctl_template_spec.rb
+++ spec/ctl_template_spec.rb
@@ -4,39 +4,40 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
-RSpec.describe 'Service Backups Ctl script' do
-  let(:renderer) do
-    Bosh::Template::Renderer.new(
-      context: BoshEmulator.director_merge(
-        YAML.load_file(manifest_file.path), 'service-backup'
-      ).to_json
-    )
+require 'bosh/template/test'
+
+RSpec.describe 'service-backup job control script' do
+  before(:all) do
+    release_path = File.join(File.dirname(__FILE__), '..')
+    release = Bosh::Template::Test::ReleaseDir.new(release_path)
+    job = release.job('service-backup')
+    @template = job.template('bin/ctl')
   end
-  let(:rendered_template) { renderer.render('jobs/service-backup/templates/ctl.erb') }
 
-  after(:each) { manifest_file.close }
+  context 'when the manifest specifies optional properties' do
+    before(:all) do
+      properties = {
+        'service-backup' => {
+          'backup_user' => 'backuper',
+          'source_folder' => '/foo'
+        }
+      }
+      @control_script = @template.render(properties)
+    end
 
-  context 'when the manifest contains a custom backup user' do
-    let(:manifest_file) { File.open('spec/fixtures/valid_with_backup_user.yml') }
+    it 'templates the specified backup_user' do
+      expect(@control_script).to include("backup_user='backuper'")
+    end
 
-    it 'templates the value of backup_user' do
-      expect(rendered_template).to include("backup_user='backuper'")
+    it 'templates the specified source_folder' do
+      expect(@control_script).to include("source_folder='/foo'")
     end
   end
 
-  context 'when the manifest does not specify a custom backup user' do
-    let(:manifest_file) { File.open('spec/fixtures/valid_s3.yml') }
-
-    it 'templates the default value' do
-      expect(rendered_template).to include("backup_user='vcap'")
-    end
-  end
-
-  context 'when the manifest specifies a source folder' do
-    let(:manifest_file) { File.open('spec/fixtures/valid_s3.yml') }
-
-    it 'templates the value of source folder' do
-      expect(rendered_template).to include("source_folder='/foo'")
+  context 'when the manifest does not specify optional properties' do
+    it 'templates the default backup_user' do
+      control_script = @template.render({})
+      expect(control_script).to include("backup_user='vcap'")
     end
   end
 end
