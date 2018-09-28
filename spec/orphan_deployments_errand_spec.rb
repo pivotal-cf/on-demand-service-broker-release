2b130eb27dccd850ac0e273a4876792612527be8
Winna Bridgewater <wbridgewater@pivotal.io>
2018-09-28 15:51:58 +0100

    Orphan deployment errand now uses a config file
    [#157692934]
    
    Signed-off-by: Derik Evangelista <devangelista@pivotal.io>
    Co-authored-by: Jack Newberry <jnewberry@pivotal.io>

--- spec/orphan_deployments_errand_spec.rb
+++ spec/orphan_deployments_errand_spec.rb
@@ -8,9 +8,9 @@
 
 require 'spec_helper'
 
-RSpec.describe 'orphan-deployments errand' do
-  let(:renderer) do
-    links = [{
+RSpec.describe 'orphan-deployments config' do
+  let(:broker_link) do
+    {
       'broker' => {
         'instances' => [
           {
@@ -23,39 +23,104 @@ RSpec.describe 'orphan-deployments errand' do
           'port' => 7070
         }
       }
-    }]
+    }
+  end
+
+  let(:renderer) do
     merged_context = BoshEmulator.director_merge(
       YAML.load_file(manifest_file),
       'orphan-deployments',
-      links
+      [broker_link]
     )
     Bosh::Template::Renderer.new(context: merged_context.to_json)
   end
 
-  let(:rendered_template) { renderer.render('jobs/orphan-deployments/templates/errand.sh.erb') }
+  let(:rendered_template) { renderer.render('jobs/orphan-deployments/templates/config.yml.erb') }
+  let(:config) { YAML.safe_load(rendered_template) }
+  let(:manifest_file) { 'spec/fixtures/orphan_deployments_basic.yml' }
 
   context 'when the broker credentials contain special characters' do
-    let(:manifest_file) { 'spec/fixtures/orphan_deployments_basic.yml' }
-
     it 'escapes the broker credentials' do
-      expect(rendered_template).to include "-brokerUsername '%username'\\''\"t:%!'"
-      expect(rendered_template).to include "-brokerPassword '%password'\\''\"t:%!'"
+      expect(config.fetch('broker_api').fetch('url')).to eq('http://123.456.789.101:7070')
+
+      basic_auth_block = config.fetch('broker_api').fetch('authentication').fetch('basic')
+      expect(basic_auth_block.fetch('username')).to eq("%username'\"t:%!")
+      expect(basic_auth_block.fetch('password')).to eq("%password'\"t:%!")
     end
   end
 
-  context 'when the broker uri is configured' do
-    let(:manifest_file) { 'spec/fixtures/orphan_deployments_basic.yml' }
+  context 'with no service instances api specified' do
+    it 'sets the service_instances_api url flag to be the management endpoint' do
+      expect(config.fetch('service_instances_api').fetch('url')).to eq('http://123.456.789.101:7070/mgmt/service_instances')
+    end
 
-    it 'uses the configured broker uri' do
-      expect(rendered_template).to include '-brokerUrl http://example.com:8080'
+    it 'sets service instances api authentication to be the broker authentication' do
+      basic_auth_block = config.fetch('service_instances_api').fetch('authentication').fetch('basic')
+      expect(basic_auth_block.fetch('username')).to eq("%username'\"t:%!")
+      expect(basic_auth_block.fetch('password')).to eq("%password'\"t:%!")
     end
   end
 
-  context 'when the broker uri property is missing' do
-    let(:manifest_file) { 'spec/fixtures/orphan_deployments_without_broker_uri.yml' }
+  context 'with service instances API specified' do
+    let(:siapi_root_ca_cert) {false}
+    let(:broker_link) do
+      bl = {
+        'broker' => {
+          'instances' => [
+            {
+              'address' => '123.456.789.101'
+            }
+          ],
+          'properties' => {
+            'username' => "%username'\"t:%!",
+            'password' => "%password'\"t:%!",
+            'port' => 8080,
+            'service_instances_api' => {
+              'authentication' => {
+                'basic' => {
+                  'username' => "myname",
+                  'password' => "supersecret"
+                }
+              },
+              'url' => 'http://example.org/give-me/some-services',
+            }
+          }
+        }
+      }
+      if siapi_root_ca_cert
+        bl['broker']['properties']['service_instances_api']['root_ca_cert'] = siapi_root_ca_cert
+      end
+      bl
+    end
 
-    it 'uses the broker job link' do
-      expect(rendered_template).to include '-brokerUrl http://123.456.789.101:7070'
+    it 'sets the service_instances_api url flag to be the configured URL' do
+      expect(config.fetch('service_instances_api').fetch('url')).to eq('http://example.org/give-me/some-services')
+    end
+
+    it 'sets correct service instances api authentication' do
+      basic_auth_block = config.fetch('service_instances_api').fetch('authentication').fetch('basic')
+      expect(basic_auth_block.fetch('username')).to eq('myname')
+      expect(basic_auth_block.fetch('password')).to eq('supersecret')
+    end
+
+    it "doesn't set root_ca_cert" do
+      expect(config.fetch('service_instances_api').key?('root_ca_cert')).to be_falsey
+    end
+
+    context 'and root_ca_cert provided' do
+      let(:siapi_root_ca_cert) do
+        '-----BEGIN CERTIFICATE-----
+          MostCERTainlyACert
+          -----END CERTIFICATE-----
+        '
+      end
+
+      it 'sets correct root_ca_cert' do
+        expect(config.fetch('service_instances_api').fetch('root_ca_cert')).to eq('-----BEGIN CERTIFICATE-----
+          MostCERTainlyACert
+          -----END CERTIFICATE-----
+        ')
+      end
     end
   end
 end
