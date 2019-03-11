e54b652ddbbb2364f8fb8e941f9137356d7ac4c8
Winna Bridgewater <wbridgewater@pivotal.io>
2019-03-11 16:01:37 +0000

    Allow service metrics binary to run against an HTTPS broker
    [#162808676]
    
    Signed-off-by: Derik Evangelista <devangelista@pivotal.io>
    Co-authored-by: Derik Evangelista <devangelista@pivotal.io>

--- spec/collect_service_metrics_script_spec.rb
+++ spec/collect_service_metrics_script_spec.rb
@@ -9,8 +9,8 @@
 require 'spec_helper'
 
 RSpec.describe 'collect-service-metrics script' do
-  let(:renderer) do
-    links = [{
+  let(:links) do
+    [{
       'broker' => {
         'instances' => [
           {
@@ -24,6 +24,9 @@ RSpec.describe 'collect-service-metrics script' do
         }
       }
     }]
+  end
+
+  let(:renderer) do
     merged_context = BoshEmulator.director_merge(YAML.load_file(manifest_file), 'service-metrics-adapter', links)
     Bosh::Template::Renderer.new(context: merged_context.to_json)
   end
@@ -47,11 +50,46 @@ RSpec.describe 'collect-service-metrics script' do
     end
   end
 
-  context 'when the broker uri property is missing' do
+  context 'when the broker uri property is missing and TLS is disabled on the broker' do
     let(:manifest_file) { 'spec/fixtures/collect_service_metrics_without_broker_uri.yml' }
 
-    it 'uses the broker job link' do
+    it 'uses the broker job link with an http prefix' do
       expect(rendered_template).to include '-brokerUrl http://123.456.789.101:7070'
     end
   end
+
+  context 'when the broker uri property is missing and TLS is enabled on the broker' do
+    let(:manifest_file) { 'spec/fixtures/collect_service_metrics_without_broker_uri_with_tls.yml' }
+
+    it 'uses the broker job link with an https prefix' do
+      expect(rendered_template).to include '-brokerUrl https://123.456.789.101:7070'
+    end
+  end
+
+  context 'disableSSLCertVerification property' do
+    context 'when disableSSLCertVerification is set to true' do
+      let(:manifest_file) { 'spec/fixtures/collect_service_metrics_with_disable_ssl_cert_verification.yml' }
+
+      it 'is passed to the command line' do
+        expect(rendered_template).to include '-disableSSLCertVerification=true'
+      end
+    end
+
+    context 'when disableSSLCertVerification is not set' do
+      let(:manifest_file) { 'spec/fixtures/collect_service_metrics_without_broker_uri.yml' }
+
+      it 'is passed to the command line as false' do
+        expect(rendered_template).to include '-disableSSLCertVerification=false'
+      end
+    end
+  end
+
+  context 'when tls.ca_cert is passed' do
+    let(:manifest_file) { 'spec/fixtures/collect_service_metrics_with_tls_certificate.yml' }
+
+    it 'is passed to the command line' do
+      expect(rendered_template).to include '-tlsCertificate "a certificate"'
+    end
+  end
+
 end
