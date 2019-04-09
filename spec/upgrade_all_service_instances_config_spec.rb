ff5d1fb844fd11e4d9aecc3bb7b789cb90e931d1
Derik Evangelista <devangelista@pivotal.io>
2019-04-09 14:30:10 +0100

    refactored upgrade-all release test
    [#159360307]
    
    Signed-off-by: Gabriele Cipriano <gcipriano@pivotal.io>
    Co-authored-by: Gabriele Cipriano <gcipriano@pivotal.io>

--- spec/upgrade_all_service_instances_config_spec.rb
+++ spec/upgrade_all_service_instances_config_spec.rb
@@ -42,31 +42,97 @@ RSpec.describe 'upgrade-all-service-instances config' do
 
   let(:config) { YAML.safe_load(rendered_template) }
 
-  context 'with no service instances api specified' do
+  context 'without any properties configured' do
     let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_minimal.yml' }
 
-    it 'is not empty' do
-      expect(config).not_to be_empty
+    it 'configures the errand with default values' do
+      expect(config.fetch('polling_interval')).to eq(60)
+      expect(config.fetch('attempt_interval')).to eq(60)
+      expect(config.fetch('attempt_limit')).to eq(5)
+      expect(config.fetch('request_timeout')).to eq(120)
+      expect(config.fetch('max_in_flight')).to eq(1)
+      expect(config.fetch('canaries')).to eq(0)
     end
 
-    it 'sets broker_api.url' do
-      expect(config.fetch('broker_api').fetch('url')).to eq('http://123.456.789.101:8080')
-    end
+    it 'configures the broker api correctly' do
+      expect(config.dig('broker_api', 'url')).to eq('http://123.456.789.101:8080')
 
-    it 'sets correct broker api authentication' do
-      basic_auth_block = config.fetch('broker_api').fetch('authentication').fetch('basic')
+      basic_auth_block = config.dig('broker_api', 'authentication', 'basic')
       expect(basic_auth_block.fetch('username')).to eq("%username'\"t:%!")
       expect(basic_auth_block.fetch('password')).to eq("%password'\"t:%!")
     end
 
-    it 'sets service_instances_api.url' do
-      expect(config.fetch('service_instances_api').fetch('url')).to eq('http://123.456.789.101:8080/mgmt/service_instances')
-    end
+    it 'configures the service_instances_api to use the mgmt endpoint to get instances' do
+      expect(config.dig('service_instances_api', 'url')).to eq('http://123.456.789.101:8080/mgmt/service_instances')
 
-    it 'sets correct service instances api authentication' do
-      basic_auth_block = config.fetch('service_instances_api').fetch('authentication').fetch('basic')
+      basic_auth_block = config.dig('service_instances_api', 'authentication', 'basic')
       expect(basic_auth_block.fetch('username')).to eq("%username'\"t:%!")
       expect(basic_auth_block.fetch('password')).to eq("%password'\"t:%!")
+    end
+  end
+
+  context 'with every property configured' do
+    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_fully_configured.yml' }
+
+    it 'configures the errand accordingly' do
+      expect(config.fetch('attempt_interval')).to eq(36)
+      expect(config.fetch('polling_interval')).to eq(101)
+      expect(config.fetch('attempt_limit')).to eq(42)
+      expect(config.fetch('max_in_flight')).to eq(13)
+      expect(config.fetch('request_timeout')).to eq(300)
+
+      expect(config.fetch('canaries')).to eq(3)
+      expect(config.fetch('canary_selection_params')).to eq(
+        'test' => true,
+        'size' => 'small'
+      )
+    end
+
+    it 'configures the broker api correctly' do
+      expect(config.dig('broker_api', 'url')).to eq('http://123.456.789.101:8080')
+
+      basic_auth_block = config.dig('broker_api', 'authentication', 'basic')
+      expect(basic_auth_block.fetch('username')).to eq("%username'\"t:%!")
+      expect(basic_auth_block.fetch('password')).to eq("%password'\"t:%!")
+    end
+
+    it 'configures the service instances api correctly' do
+      expect(config.dig('service_instances_api', 'url')).to eq('http://123.456.789.101:8080/mgmt/service_instances')
+
+      basic_auth_block = config.dig('service_instances_api', 'authentication', 'basic')
+      expect(basic_auth_block.fetch('username')).to eq("%username'\"t:%!")
+      expect(basic_auth_block.fetch('password')).to eq("%password'\"t:%!")
+    end
+
+    describe 'attempt limit property' do
+      let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_invalid_attempt_limit.yml' }
+
+      it 'fails when it is less or equal zero' do
+        expect { rendered_template }.to raise_error(
+          RuntimeError,
+          'Invalid upgrade_all_service_instances.attempt_limit - must be greater or equal 1'
+        )
+      end
+    end
+
+    describe 'max_in_flight property' do
+      let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_invalid_max_in_flight.yml' }
+
+      it 'fails when is less than 1' do
+        expect do
+          rendered_template
+        end.to raise_error(RuntimeError, 'Invalid upgrade_all_service_instances.max_in_flight - must be greater or equal 1')
+      end
+    end
+
+    describe 'canaries property' do
+      let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_invalid_canaries.yml' }
+
+      it 'fails if its less than zero' do
+        expect do
+          rendered_template
+        end.to raise_error(RuntimeError, 'Invalid upgrade_all_service_instances.canaries - must be greater or equal 0')
+      end
     end
   end
 
@@ -150,141 +216,6 @@ RSpec.describe 'upgrade-all-service-instances config' do
           -----END CERTIFICATE-----
         ')
       end
-    end
-  end
-
-  context 'with polling interval provided' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_with_polling_interval.yml' }
-
-    it 'sets polling interval to 101' do
-      expect(config.fetch('polling_interval')).to eq(101)
-    end
-  end
-
-  context 'without polling interval provided' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_minimal.yml' }
-
-    it 'sets polling interval to the default' do
-      expect(config.fetch('polling_interval')).to eq(60)
-    end
-  end
-
-  context 'with attempt interval provided' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_with_attempt_interval.yml' }
-
-    it 'sets polling interval to 36' do
-      expect(config.fetch('attempt_interval')).to eq(36)
-    end
-  end
-
-  context 'without attempt interval provided' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_minimal.yml' }
-
-    it 'sets attempt interval to the default' do
-      expect(config.fetch('attempt_interval')).to eq(60)
-    end
-  end
-
-  context 'with attempt limit provided' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_with_attempt_limit.yml' }
-
-    it 'sets attempt limit to 42' do
-      expect(config.fetch('attempt_limit')).to eq(42)
-    end
-  end
-
-  context 'without attempt limit provided' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_minimal.yml' }
-
-    it 'sets attempt limit to the default' do
-      expect(config.fetch('attempt_limit')).to eq(5)
-    end
-  end
-
-  context 'with an attempt limit less or equal zero' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_invalid_attempt_limit.yml' }
-
-    it 'fails' do
-      expect do
-        rendered_template
-      end.to raise_error(RuntimeError, 'Invalid upgrade_all_service_instances.attempt_limit - must be greater or equal 1')
-    end
-  end
-
-  context 'with max in flight provided' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_with_max_in_flight.yml' }
-
-    it 'sets max in flight to 13' do
-      expect(config.fetch('max_in_flight')).to eq(13)
-    end
-  end
-
-  context 'without max in flight provided' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_minimal.yml' }
-
-    it 'sets max in flight to the default of 1' do
-      expect(config.fetch('max_in_flight')).to eq(1)
-    end
-  end
-
-  context 'with an max in flight less or equal zero' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_invalid_max_in_flight.yml' }
-
-    it 'fails' do
-      expect do
-        rendered_template
-      end.to raise_error(RuntimeError, 'Invalid upgrade_all_service_instances.max_in_flight - must be greater or equal 1')
-    end
-  end
-
-  context 'when canaries are configured incorrectly' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_invalid_canaries.yml' }
-
-    it 'fails if its less than zero' do
-      expect do
-        rendered_template
-      end.to raise_error(RuntimeError, 'Invalid upgrade_all_service_instances.canaries - must be greater or equal 0')
-    end
-  end
-
-  context 'when canaries are configured' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_minimal.yml' }
-
-    it 'ouputs the default value' do
-      expect(config.fetch('canaries')).to eq(0)
-    end
-  end
-
-  context 'when canaries and the canary_selection_params are configured' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_canaries_and_canary_selection_params.yml' }
-
-    it 'both are available in the config' do
-      expect(config.fetch('canaries')).to eq(3)
-      expect(config.fetch('canary_selection_params')).to eq('test' => true, 'size' => 'small')
-    end
-  end
-
-  context 'when canary_selection_params are configured' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_canary_selection_params.yml' }
-
-    it 'succeeds' do
-      expect(config.fetch('canary_selection_params')).to eq('test' => true, 'size' => 'small')
-    end
-  end
-
-  context 'without request timeout provided' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_minimal.yml' }
-
-    it 'sets request timeout to 120' do
-      expect(config.fetch('request_timeout')).to eq(120)
-    end
-  end
-
-  context 'with request timeout provided' do
-    let(:manifest_file) { File.open 'spec/fixtures/upgrade_all_with_request_timeout.yml' }
-
-    it 'sets request timeout to the provided value' do
-      expect(config.fetch('request_timeout')).to eq(300)
     end
   end
 end
