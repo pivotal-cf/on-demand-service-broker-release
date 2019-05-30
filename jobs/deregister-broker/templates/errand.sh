f1840a8be914195b72a3aca8335fe60863b4f220
Felisia Martini <fmartini@pivotal.io>
2019-05-30 16:00:37 +0100

    Configure deregister-broker to use bpm
    [#164139969]
    
    Signed-off-by: Winna Bridgewater <wbridgewater@pivotal.io>
    Co-authored-by: Winna Bridgewater <wbridgewater@pivotal.io>

--- jobs/deregister-broker/templates/errand.sh
+++ jobs/deregister-broker/templates/errand.sh
@@ -1,24 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
 # Copyright (C) 2016-Present Pivotal Software, Inc. All rights reserved.
 # This program and the accompanying materials are made available under the terms of the under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 # http://www.apache.org/licenses/LICENSE-2.0
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
 
----
-name: deregister-broker
-
-packages:
-  - deregister-broker
-
-templates:
-  errand.sh: bin/run
-  config.yml.erb: config/config.yml
-  bpm.yml.erb: config/bpm.yml
-
-consumes:
-  - name: broker
-    type: broker
-
-properties:
-  broker_name:
-    description: name of broker to deregister with cloud controller
+/var/vcap/packages/bpm/bin/bpm run deregister-broker 
